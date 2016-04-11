87cc579af64c5dd5f83602ebb71e9efeef81bedf
AlejandroLariosUlate <Alexlarios09@hotmail.com>
2016-04-10 21:02:02 -0600

    Added files via upload

--- __init__.py
+++ __init__.py
@@ -1,118 +1,97 @@
 __author__ = "Alejandro"
-__date__ = "$10-abr-2016 19:44:14$"
+__date__ = "$10-abr-2016 19:43:25$"
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#  Menu.py
-from Empleado import Empleado
-import Operaciones
-class Menu:
+#  Operaciones.py
+empleados=[]
+posicion=0
+
+def agregar_empleado(empleado_nuevo):
+	empleados.append(empleado_nuevo)
+	informacion=("SE AGREGO EXITOSAMENTE\n\n")
+	return informacion
 	
+def mostrar_empleados(self):
+	informacion="Lista de empleados:  \n"
+	for empleado in empleados:
+		informacion += str(empleado.getInformacion())+"\n"
+	return informacion
 	
-	def menu(self):
+def empleado_especifico():
+	informacion="Empleado: \n"
+	informacion+=empleados[posicion].getInformacion()
+	return informacion
+	
+def verificar_empleado(codigo):
+	resultado=False #no existe empleado con ese codigo
+	for i in range(len(empleados)):
+		if (empleados[i].getCodigo()==codigo):
+			resultado=True
+			global posicion
+			posicion=i
+	return resultado
+	
+def mostrar_salario():
+	salario_ordinario=empleados[posicion].getHoras_ordinarias()* empleados[posicion].getMonto_hora()
+	salario_extra=empleados[posicion].getHoras_extras()*(empleados[posicion].getMonto_hora()*1.5)
+	salario_bruto=salario_extra+salario_ordinario
+	deducciones= salario_bruto*0.0917
+	salario_neto=salario_bruto- deducciones
+	
+	return str(salario_neto)
+	
+def mostrar_planilla():
+	for i in range (0,len(empleados)):
+		salario_ordinario=empleados[i].getHoras_ordinarias()* empleados[i].getMonto_hora()
+		salario_extra=empleados[i].getHoras_extras()*(empleados[i].getMonto_hora()*1.5)
+		salario_bruto=salario_extra+salario_ordinario
+		deducciones= salario_bruto*0.0917
+		salario_neto=salario_bruto- deducciones
 		
-		opcion=0
-		opcion2=0
-		
-		while(opcion<9):
-			
-			print("""
-                    1. Ingresar un empleado nuevo.
-                    2. Listado general de empleados.
-                    3. Mostrar información de un empleado específico.
-                    4. Modificar un empleado existente.
-                    5. Mostrar el salario de un empleado.
-                    6. Calcular el monto de planilla de cada empleado.
-                    7. Calcular y mostrar el monto de total de egresos
-                    8. Calcular y mostrar la cantidad de horas laboradas
-                    9. Salir	
-			""")
-			
-			opcion=int(input("Digite una opcion   > "))
-			
-			if(opcion ==1):
-				codigo=int (input("Digite el codigo     "))
-				nombre=input("Digite el nombre     ")
-				fecha_Nacimiento=input("Digite la fecha de nacimiento     ")
-				numero_departamento=int (input("Digite el numero de departamento     "))
-				codigo_jefe=int (input("Digite el codigo del jefe     "))
-				horas_extras=int (input("Digite las horas extras     "))
-				horas_ordinarias=int (input("Digite las horas ordinarias     "))
-				monto_hora=int (input("Digite el monto de la hora     "))
-				print("\n\n\n")
-                
-				empleado_nuevo=Empleado(codigo, nombre, fecha_Nacimiento, numero_departamento, codigo_jefe, horas_extras, horas_ordinarias, monto_hora)
-				print (Operaciones.agregar_empleado(empleado_nuevo))
-				
-			elif(opcion ==2):
-				print (Operaciones.mostrar_empleados(self))
-				
-			elif(opcion==3):
-				codigo=int (input("Digite el codigo     "))
-				if(Operaciones.verificar_empleado(codigo)):
-					print(Operaciones.empleado_especifico())
-				else:
-					print("Este codigo de empleado no existe")
-			
-			elif(opcion ==4):
-				codigo=int (input("Digite el codigo"))
-				if(Operaciones.verificar_empleado(codigo)):
-					print("""
-						Que desea Cambiar:
-						
-						1. Nombre
-						2. Fecha de nacimiento
-						3. Numero de departamento
-						4. Codigo del jefe.
-						5. Horas extras
-						6. Horas ordinarias
-						7. Monto hora
-						
-					""")
-					opcion2=int(input("Digite una opcion > "))
-					
-					if(opcion2==1):
-						nombre=input("Digite el nombre    ")
-						Operaciones.cambiar_nombre(nombre)
-					elif(opcion2==2):
-						fecha_Nacimiento=input("Digite la fecha de nacimiento    ")
-						Operaciones.cambiar_Fecha(fecha_Nacimiento)
-					elif(opcion2==3):
-						numero_departamento=int(input("Digite el numero del departamento    "))
-						Operaciones.cambiar_departamento(numero_departamento)
-					elif(opcion2==4):
-						codigo_jefe=int(input("Digite el codigo del jefe     "))
-						Operaciones.cambiar_codigoJefe(codigo_jefe)
-					elif(opcion2==5):
-						horas_extras=int(input("Digite la cantidad de horas extras      "))
-						Operaciones.cambiarHoras_extras(horas_extras)
-					elif(opcion2==6):
-						horas_ordinarias=int(input("Digite la cantidad de horas ordinarias      "))
-						Operaciones.cambiarHoras_ordinarias(horas_ordinarias)
-					elif(opcion2==7):
-						monto_hora=int(input("Digite el monto de hora     "))
-						Operaciones.cambiarMonto_hora(monto_hora)
-						
-				else:
-					print("El codigo que usted digito no pertenece a ningun empleado")
-					
-			elif(opcion==5):
-				codigo=int (input("Digite el codigo"))
-				if(Operaciones.verificar_empleado(codigo)):
-					print ("Salario: "+Operaciones.mostrar_salario())
-				else:
-					print("El codigo que usted digito no pertenece a ningun empleado")
-				
-			elif(opcion==6):
-				print (Operaciones.mostrar_planilla())
-				
-			elif(opcion==7):
-				print (Operaciones.mostrar_egresos())
-				
-			elif(opcion==8):
-				print (Operaciones.mostrar_horasLaboradas(self))
-				
-			elif(opcion==9):
-				SystemExit(0)
-				
-menu=Menu()
-menu.menu()
+		info="Codigo: " +str(empleados[i].getCodigo())+"\nNombre: "+empleados[i].getNombre() + "\nSalario bruto: "+str(salario_bruto)+"\nSalario neto: "+str(salario_neto)+"\nDeducciones: "+str(deducciones)
+	return info
+	
+def cambiar_nombre(nombre):
+	empleados[posicion].setNombre(nombre)
+
+def cambiar_Fecha(fecha_Nacimiento):
+	empleados[posicion].setFecha_Nacimiento(fecha_Nacimiento)
+
+def cambiar_departamento(numero_departamento):
+	empleados[posicion].setNumero_departamento(numero_departamento)
+	
+def cambiar_codigoJefe(codigo_jefe):
+	empleados[posicion].setCodigo_jefe(codigo_jefe)
+	
+def cambiarHoras_extras(horas_extras):
+	empleados[posicion].setHoras_extras(horas_extras)
+	
+def cambiarHoras_ordinarias(horas_ordinarias):
+	empleados[posicion].setHoras_ordinarias(horas_ordinarias)
+	
+def cambiarMonto_hora(monto_hora):
+	empleados[posicion].setMonto_horas(monto_hora)
+	
+def mostrar_egresos():
+	contador=0
+	for i in range (0,len(empleados)):
+		salario_ordinario=empleados[i].getHoras_ordinarias()* empleados[i].getMonto_hora()
+		salario_extra=empleados[i].getHoras_extras()*(empleados[i].getMonto_hora()*1.5)
+		salario_bruto=salario_extra+salario_ordinario
+		deducciones= salario_bruto*0.0917
+		salario_neto=salario_bruto- deducciones
+		contador+=salario_neto
+	informacion="El total de egresos es: "+ str(contador)
+	return informacion
+	
+def mostrar_horasLaboradas(self):
+	contador_ordinarias=0
+	contador_extras=0
+	informacion=""
+	for i in range(0,len(empleados)):
+		contador_ordinarias+=empleados[i].getHoras_ordinarias()
+		contador_extras+=empleados[i].getHoras_extras()
+	informacion+=("La cantidad de horas extras trabajadas de todos los empleados es: "+str(contador_extras)+
+	"\nLa cantidad de horas ordinarias trabajadas de todos los empleados es: "+str(contador_ordinarias))
+	return informacion
+	
